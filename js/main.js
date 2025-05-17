5c596ea868a3b0e8b5f95156ffe877ab93f07645
amritchapai <chapaiamrit@gmail.com>
2025-05-17 14:41:03 +0545

    protfolio section completed

--- js/main.js
+++ js/main.js
@@ -8,6 +8,28 @@ const companyLogos = [
   "./assets/protonet.png",
 ];
 
+//for tracking methods
+const trackingMethod = [
+  {
+    Color: "blue",
+    title: "Connect wallets & companies",
+    description:
+      "Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of. ",
+  },
+  {
+    Color: "orange",
+    title: "Review transactions",
+    description:
+      "Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of. ",
+  },
+  {
+    Color: "yellow",
+    title: "Get income insights",
+    description:
+      "Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of. ",
+  },
+];
+
 console.log("hello");
 document.addEventListener("DOMContentLoaded", () => {
   const clientLogoSection = document.getElementById("client-logos");
@@ -18,5 +40,25 @@ document.addEventListener("DOMContentLoaded", () => {
       .join("");
   } else {
     console.error("Element with ID 'client-logos' not found.");
+  }
+
+  const trackingSection = document.getElementById("tracking-methods");
+  if (trackingSection) {
+    trackingSection.innerHTML = trackingMethod
+      .map(
+        (item, index) => `<div class="tracking-card">
+            <div>
+              <div class="upper-box"></div>
+              <div class="lower-box ${item.Color}"></div>
+            </div>
+            <h2>${item.title}</h2>
+            <p>
+              Indulgence way everything joy alteration boisterous the
+              attachment. Party we years to order allow asked of.
+            </p>
+            <div class="number">0${index + 1}</div>
+          </div>`
+      )
+      .join("");
   }
 });
